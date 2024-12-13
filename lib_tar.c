65950323d687c340bb109d372e02a2e18d3581e5
alvimavila <guilherme.alvimavila@student.uclouvain.be>
2024-12-13 17:16:13 +0100

    Added functions in lib_tar.c until is_symlink

--- lib_tar.c
+++ lib_tar.c
@@ -1,4 +1,25 @@
 #include "lib_tar.h"
+#define TAR_CHECKSUM_SIZE 8
+#define TAR_MAGIC_OFFSET 257
+#define TAR_VERSION_OFFSET 263
+#define TAR_CHECKSUM_OFFSET 148
+#define TAR_BLOCK_SIZE 512
+#define TAR_NAME_SIZE 100
+#define TAR_TYPEFLAG_OFFSET 156
+
+// Function to calculate the checksum to make sure the checksum in the header is correct
+
+int calculate_checksum(const uint8_t *header) {
+    int checksum = 0;
+    for (int i = 0; i < TAR_BLOCK_SIZE; i++) {
+        if (i >= TAR_CHECKSUM_OFFSET && i < TAR_CHECKSUM_OFFSET + TAR_CHECKSUM_SIZE) {
+            checksum += 32;
+        } else {
+            checksum += header[i];
+        }
+    }
+    return checksum;
+}
 
 /**
  * Checks whether the archive is valid.
@@ -16,21 +37,90 @@
  *         -3 if the archive contains a header with an invalid checksum value
  */
 int check_archive(int tar_fd) {
-    return 0;
+
+    uint8_t buffer[TAR_BLOCK_SIZE];
+    int header_count = 0;
+
+    while (read(tar_fd, buffer, TAR_BLOCK_SIZE) == TAR_BLOCK_SIZE) {
+        
+        int is_null_block = 1;
+        for (int i = 0; i < TAR_BLOCK_SIZE; i++) {
+            if (buffer[i] != 0) {
+                is_null_block = 0;
+                break;
+            }
+        }
+        if (is_null_block) {
+            break;
+        }
+
+        // Vérifie la valeur magique
+        if (strncmp((char *)(buffer + TAR_MAGIC_OFFSET), TMAGIC, TMAGLEN) != 0) {
+            return -1;
+        }
+
+        // Vérifie la version
+        if (strncmp((char *)(buffer + TAR_VERSION_OFFSET), TVERSION, TVERSLEN) != 0) {
+            return -2;
+        }
+
+        // Vérifie le checksum
+        char stored_checksum_str[TAR_CHECKSUM_SIZE + 1];
+        memcpy(stored_checksum_str, buffer + TAR_CHECKSUM_OFFSET, TAR_CHECKSUM_SIZE);
+        stored_checksum_str[TAR_CHECKSUM_SIZE] = '\0';
+        int stored_checksum = (int)strtol(stored_checksum_str, NULL, 8);
+
+        int computed_checksum = calculate_checksum(buffer);
+        if (stored_checksum != computed_checksum) {
+            return -3;
+        }
+        header_count++;
+    }
+
+    return header_count;
+
 }
 
-/**
- * Checks whether an entry exists in the archive.
- *
- * @param tar_fd A file descriptor pointing to the start of a valid tar archive file.
- * @param path A path to an entry in the archive.
- *
- * @return zero if no entry at the given path exists in the archive,
- *         any other value otherwise.
- */
-int exists(int tar_fd, char *path) {
+// Just a function to regroup "is_dir", "is_file", "is_symlink" because they are very similar
+int is_smth(int tar_fd, char *path, char *type){
+
+        uint8_t buffer[TAR_BLOCK_SIZE];
+
+    // Go back to the beginning of the tar
+    if (lseek(tar_fd, 0, SEEK_SET) == -1) {
+        perror("lseek");
+        return 0;
+    }
+
+    while (read(tar_fd, buffer, TAR_BLOCK_SIZE) == TAR_BLOCK_SIZE) {
+
+        int is_null_block = 1;
+        for (int i = 0; i < TAR_BLOCK_SIZE; i++) {
+            if (buffer[i] != 0) {
+                is_null_block = 0;
+                break;
+            }
+        }
+        if (is_null_block) {
+            break;
+        }
+
+        char name[TAR_NAME_SIZE + 1];
+        memcpy(name, buffer, TAR_NAME_SIZE);
+        name[TAR_NAME_SIZE] = '\0';
+
+        if (strcmp(name, path) == 0) {
+            // Verify if it's the right type
+            char typeflag = buffer[TAR_TYPEFLAG_OFFSET];
+            if (typeflag == type) {
+                return 1;
+            }
+            return 0;
+        }
+    }
     return 0;
-}
+} 
+
 
 /**
  * Checks whether an entry exists in the archive and is a directory.
@@ -42,7 +132,7 @@ int exists(int tar_fd, char *path) {
  *         any other value otherwise.
  */
 int is_dir(int tar_fd, char *path) {
-    return 0;
+    return is_smth(tar_fd, path, DIRTYPE);
 }
 
 /**
@@ -55,7 +145,7 @@ int is_dir(int tar_fd, char *path) {
  *         any other value otherwise.
  */
 int is_file(int tar_fd, char *path) {
-    return 0;
+    return is_smth(tar_fd, path, REGTYPE);
 }
 
 /**
@@ -67,7 +157,7 @@ int is_file(int tar_fd, char *path) {
  *         any other value otherwise.
  */
 int is_symlink(int tar_fd, char *path) {
-    return 0;
+    return is_smth(tar_fd, path, SYMTYPE);
 }
 
 
